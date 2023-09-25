492be6ba6066bb65c7242c80fd4a4905a4605e33
Jeevan_gitactions <jeevaneg2004@gmail.com>
2023-09-25 12:22:42 +0530

    first commit

---
+++ bubble-sort.py
@@ -0,0 +1,8 @@
+# arr=[]
+# n=int(input("Enter the size of the array:"))
+# for i in range(0,n):
+#     num=int(input("Enter the %d array Element: ",i))
+#     arr.append(num)
+# for i in range(0,n-1):
+#     for i in range(0,n-1-i):
+#         if()
